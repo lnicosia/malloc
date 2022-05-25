70f80a2e23af6822366f9f95284cb35fb49169ad
lnicosia <lnicosia@student.42.fr>
2022-05-25 18:02:11 +0200

    [ADD] Put back libft as a submodule.

--- libft/src/ft_ceil.c
+++
@@ -1,6 +0,0 @@
-double	ft_ceil(double nb)
-{
-	if (nb > (long long int)nb)
-		return (double)((long long int)nb + 1);
-	return nb;
-}		
--- libft/src/ft_centiceil.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-double	ft_centiceil(double nb)
-{
-    nb *= 10;
-    nb = ft_ceil(nb);
-    return nb / 10.0;
-}
